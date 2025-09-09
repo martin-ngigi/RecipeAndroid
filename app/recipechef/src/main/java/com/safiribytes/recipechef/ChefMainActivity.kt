c151164dc5452551418c1c4b49941c7064f81a2a
wainaina-cb <mwainaina@co-opbank.co.ke>
2025-09-09 17:12:18 +0300

    Added Delay Splash Screen

--- app/recipechef/src/main/java/com/safiribytes/recipechef/ChefMainActivity.kt
+++ app/recipechef/src/main/java/com/safiribytes/recipechef/ChefMainActivity.kt
@@ -10,12 +10,29 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.lifecycle.lifecycleScope
 import com.safiribytes.coreui.theme.userapp.UserAppTheme
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class ChefMainActivity : ComponentActivity() {
+    @Volatile
+    private var keepSplash = true  // start as true
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        // Install splash before super.onCreate
+        val splashScreen = installSplashScreen()
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
+        // Keep splash visible while condition is true
+        splashScreen.setKeepOnScreenCondition { keepSplash }
+
+        // Delay splash for 3 seconds
+        lifecycleScope.launch {
+            delay(2000)
+            keepSplash = false
+        }
         setContent {
             UserAppTheme {
                 UserMainScreen()
--- app/recipeuser/src/main/java/com/safiribytes/recipeuser/UserMainActivity.kt
+++ app/recipeuser/src/main/java/com/safiribytes/recipeuser/UserMainActivity.kt
@@ -1,6 +1,7 @@
 package com.safiribytes.recipeuser
 
 import android.os.Bundle
+import android.os.PersistableBundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -10,11 +11,33 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.lifecycle.lifecycleScope
 import com.safiribytes.coreui.theme.userapp.UserAppTheme
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class UserMainActivity : ComponentActivity() {
+
+    @Volatile
+    private var keepSplash = true  // start as true
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        // Install splash before super.onCreate
+        val splashScreen = installSplashScreen()
         super.onCreate(savedInstanceState)
+
+        // Keep splash visible while condition is true
+        splashScreen.setKeepOnScreenCondition { keepSplash }
+
+        // Delay splash for 3 seconds
+        lifecycleScope.launch {
+            delay(2000)
+            keepSplash = false
+        }
+
         enableEdgeToEdge()
         setContent {
             UserAppTheme {
@@ -23,7 +46,6 @@ class UserMainActivity : ComponentActivity() {
         }
     }
 }
-
 @Composable
 fun UserMainScreen() {
     Scaffold(
